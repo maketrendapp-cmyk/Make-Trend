72f212ab20cf8d42d296e14c656e72160d876cbe
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-07-11 03:49:22 +0545

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 // frontend/pages/_app.js
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { AuthProvider } from '../context/AuthContext';
 import Navbar from '../components/Navbar';
@@ -11,13 +11,23 @@ function MyApp({ Component, pageProps }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
 
-  // No loader needed - instant SPA
+  // List of pages where bottom nav should be hidden (optional)
+  // const hideBottomNav = ['/auth/login', '/auth/register'];
+  // const shouldHideBottomNav = hideBottomNav.includes(router.pathname);
+
   return (
     <AuthProvider>
       <div className="min-h-screen bg-bg pb-20 md:pb-0">
+        {/* Navbar - visible on all pages */}
         <Navbar />
+        
+        {/* Main page content */}
         <Component {...pageProps} />
+        
+        {/* Bottom Navigation - visible on all pages except auth pages */}
         <BottomNav onMenuToggle={() => setIsMenuOpen(true)} />
+        
+        {/* Slide-in Menu */}
         <Menu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
       </div>
     </AuthProvider>
