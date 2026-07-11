b06c456322ac06e570ab03c1ae278bdf4ecd0302
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-07-11 14:28:18 +0545

    Create admin-app/pages/_app.js

---
+++ admin-app/pages/_app.js
@@ -0,0 +1,13 @@
+// pages/_app.js
+import { AuthProvider } from '../components/Auth';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
