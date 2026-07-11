e53f63d52b7ac55121e70404f89402ca18b042d8
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-07-11 14:30:40 +0545

    Create tailwind.config.js

---
+++ admin-app/tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: { colors: { primary: '#4F46E5', border: '#E5E7EB' } } },
+  plugins: [],
+};
