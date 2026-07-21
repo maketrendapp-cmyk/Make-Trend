0fa038316044337209d27ae07c35a35ee726fc07
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-07-21 10:16:31 +0545

    Update Utils.js

--- frontend/utils/Utils.js
+++ frontend/utils/Utils.js
@@ -1,1 +1,9 @@
-ntg
+// utils/deviceId.js
+export function getDeviceId() {
+  let deviceId = localStorage.getItem('deviceId');
+  if (!deviceId) {
+    deviceId = crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    localStorage.setItem('deviceId', deviceId);
+  }
+  return deviceId;
+}
