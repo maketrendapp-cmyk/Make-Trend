1b200e3896c81f016b0416ec73872a7265a921e3
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-08-06 10:32:49 +0545

    Create edit/[id].js

---
+++ frontend/pages/productstrend/edit/[id].js
@@ -0,0 +1,4 @@
+// pages/productstrend/edit/[id].js
+// This page re-uses the launch page with edit mode enabled.
+// The `id` from the URL will be available in `launch.js` via `router.query.id`.
+export { default } from '../launch';
